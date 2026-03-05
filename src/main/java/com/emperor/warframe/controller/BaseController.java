1652e515007681d1c6f3f1b14097972aa804c7b8
Yoni Cant <yoni.cant@student.kdg.be>
2026-03-05 14:55:10 +0100

    IP check

--- src/main/java/com/emperor/warframe/controller/BaseController.java
+++ src/main/java/com/emperor/warframe/controller/BaseController.java
@@ -2,8 +2,8 @@ package com.emperor.warframe.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("${apiPrefix}/base")
@@ -13,5 +13,11 @@ public class BaseController {
     @GetMapping
     String getBase() {
         return MESSAGE;
+    }
+
+    @GetMapping("/test-ip")
+    public String getMyIp() {
+        // This fetches your own outbound IP from a 3rd party service
+        return new RestTemplate().getForObject("https://api.ipify.org", String.class);
     }
 }
